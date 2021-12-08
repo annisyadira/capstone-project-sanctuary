9f7f56f81e519264da561c74799b3fa092d387da
Selvia Eldina <73385850+selviaeld@users.noreply.github.com>
2021-12-08 21:19:06 +0700

    Update routes.js

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,13 +3,15 @@ import { SignIn } from '../views/pages/signin';
 import Article from '../views/pages/article';
 import Story from '../views/pages/story';
 import WriteStory from '../views/pages/writestory';
+import AboutUs from '../views/pages/aboutus';
 
 const routes = {
   '/signup': SignUp,
   '/signin': SignIn,
   '/article': Article,
   '/story': Story,
-  '/writestory': WriteStory
+  '/writestory': WriteStory,
+  '/aboutus': AboutUs,
 };
 
 export default routes;
