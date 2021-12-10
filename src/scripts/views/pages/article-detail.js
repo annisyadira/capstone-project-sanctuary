352af37a6d3f4986840f4d909da33a83814bd0bb
Selvia Eldina <73385850+selviaeld@users.noreply.github.com>
2021-12-10 11:42:39 +0700

    Create article-detail.js

---
+++ src/scripts/views/pages/article-detail.js
@@ -0,0 +1,22 @@
+import { createArticleDetailTemplate } from '../templates/template-creator';
+import data from '../../data/DATA.json';
+
+const ArticleDetail = {
+    async render () {
+        return `
+        <h3 class='title'>ARTIKEL</h3>
+        <p class='sub-title'>Ketahui lebih banyak tentang pentingnya menjaga kesehatan mental,</br>tanda-tanda awal penyakit mental, cara mengatasi penyakit mental, dan masih banyak lagi!<p>
+        <div id='content' class='container'></div>
+        `;
+    },
+
+    async afterRender() {
+        const articles = data[0].article;
+        const container = document.querySelector('#content');
+
+        articles.forEach((article) => {
+            container.innerHTML += createArticleDetailTemplate(article);
+        });
+    },
+}
+export default ArticleDetail;
