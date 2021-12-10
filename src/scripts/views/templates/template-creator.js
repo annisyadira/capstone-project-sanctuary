ba8306044b042e20014abb13245ab03fbccb8bc5
Selvia Eldina <73385850+selviaeld@users.noreply.github.com>
2021-12-10 08:35:34 +0700

    add about us

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -49,10 +49,44 @@ const createSignUp = () => `
     <button type='submit' class='btn btn-dark btn-lg' id='submitBtn'>Daftar</button>
 `;
 
+const createAboutUs = () => `
+    <h2 style="text-align:center">OUR TEAM</h2>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <center>
+                    <img src="./images/annisya.jpg" alt="Annisya" style="width:70%">
+                </center>
+                <div class="container">
+                    <h2>Annisya Dira Prastiwi</h2>
+                    <p class="title">Front-End and Back-End Developer</p>
+                    <p>Seorang mahasiswi yang memiliki ketertarikan dalam bidang IT</p>
+                    <a class="button" href="https://github.com/annisyadira" target="_blank" rel="noopener">Contact</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <center>
+                    <img src="./images/via.jpg" alt="Via" style="width:70%">
+                </center>
+                <div class="container">
+                    <h2>Selvia Eldina</h2>
+                    <p class="title">Front-End and Back-End Developer</p>
+                    <p>Seorang mahasiswi yang memiliki ketertarikan dalam bidang IT</p>
+                    <a class="button" href="https://www.linkedin.com/in/selvia-eldina-35840621b/" target="_blank" rel="noopener">Contact</a>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
 export { 
     createArticleItem,
     createStoryItem,
     createInputStory,
     createSignIn,
-    createSignUp 
+    createSignUp,
+    createAboutUs
 };
