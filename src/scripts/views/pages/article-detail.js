75cc8beb78982cdd3f4fc0fdb66445e2ac7d82b4
Selvia Eldina <73385850+selviaeld@users.noreply.github.com>
2021-12-13 09:32:51 +0700

    update detail artikel

--- src/scripts/views/pages/article-detail.js
+++ src/scripts/views/pages/article-detail.js
@@ -1,22 +1,30 @@
 import { createArticleDetailTemplate } from '../templates/template-creator';
 import data from '../../data/DATA.json';
+import UrlParser from '../../routes/url-parser';
 
 const ArticleDetail = {
     async render () {
         return `
         <h3 class='title'>ARTIKEL</h3>
         <p class='sub-title'>Ketahui lebih banyak tentang pentingnya menjaga kesehatan mental,</br>tanda-tanda awal penyakit mental, cara mengatasi penyakit mental, dan masih banyak lagi!<p>
-        <div id='content' class='container'></div>
+        <div id='article-detail' class='container'></div>
         `;
     },
 
     async afterRender() {
-        const articles = data[0].article;
-        const container = document.querySelector('#content');
-
-        articles.forEach((article) => {
-            container.innerHTML += createArticleDetailTemplate(article);
+        window.scrollTo(0, 0);
+        const getArticle = data[0].article;
+        
+        const url = UrlParser.parseActiveUrlWithoutCombiner();
+        const articleId = await url.id;
+        const getIndex = getArticle.findIndex(function(item, i) {
+            return item.id_article === articleId;
         });
+        
+        const article = getArticle.at(getIndex);
+        
+        const container = document.querySelector('#article-detail');
+        container.innerHTML = createArticleDetailTemplate(article);
     },
 }
 export default ArticleDetail;
