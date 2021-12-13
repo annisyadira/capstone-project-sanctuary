82cb735b9b5c1a548adc345b70c413f8d919a50e
Selvia Eldina <73385850+selviaeld@users.noreply.github.com>
2021-12-13 16:42:24 +0700

    added locator page

---
+++ src/scripts/views/pages/locator.js
@@ -0,0 +1,11 @@
+const Locator = {
+    async render() {
+        return `
+        <h3 class='title'>LOKASI</h3>
+        <p class='sub-title'>Temukan lokasi Psikolog, Psikiater, atau Pusat Kesehatan Masyarakat terdekat<p>
+        <div id='body' class='container'></div>
+        `;
+    },
+}
+
+export default Locator;
